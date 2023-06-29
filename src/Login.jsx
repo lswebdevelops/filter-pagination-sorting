95854a774cb971a531da2ca971308d30f7cdc00f
lswebdevelops <sardanhaluciano9690@gmail.com>
2023-06-29 20:50:27 -0300

    getting access to passwod and email from a form using action

--- src/Login.jsx
+++ src/Login.jsx
@@ -1,9 +1,15 @@
 import React from "react";
 import { useNavigate, Form } from "react-router-dom";
 
-export async function action(obj) {
-    console.log(obj)
-    return null
+export async function action({ request }) {
+  const formData = await request.formData()
+  const email = formData.get("email")
+  const password = formData.get("password")
+  // process this info however I wanted 
+  // pass the email and password to the loginuser function 
+  console.log(email, password);
+  
+  return null
 }
 
 export default function Login() {
--- src/index.js
+++ src/index.js
@@ -30,7 +30,7 @@ const router = createBrowserRouter(createRoutesFromElements(
       }}
     />
     <Route 
-      path="login" 
+      path="login/:id" 
       element={<Login />}
       action={loginAction}
     />
